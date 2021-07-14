98cb8fde91e86eeee4f46371336ba942147d4cee
P4sc4l94 <76444140+P4sc4l94@users.noreply.github.com>
2021-07-14 18:33:33 -0400

    Update users.js
    Added three new routes for the profiles page and the edit user info page

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+const {isLoggedIn, isAuthor, validateCamp} = require('../middleware');
 
 const users = require('../controllers/users') //holds the functions/logic for the users routes
 
@@ -22,5 +23,14 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
 
 //log out user
 router.get('/logout', users.logout);
+
+//displays user's profile page
+router.get('/profile/:id', catchAsync(users.showProfile));
+
+//show user profile edit page
+router.get('/edit/:id', catchAsync(users.renderEditProfile));
+
+//post updated user profile info
+router.put('/profile/:id', isLoggedIn, catchAsync(users.editProfile))
 
 module.exports = router;

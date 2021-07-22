aa4f735af4c922d5f03108d2c58110d493f7be93
P4sc4l94 <76444140+P4sc4l94@users.noreply.github.com>
2021-07-22 15:24:18 -0400

    Update campground.js
    updated to match local

--- models/campground.js
+++ models/campground.js
@@ -50,7 +50,8 @@ const CampgroundSchema = new Schema({
 CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
   return `
   <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
-  <p>${this.description.substring(0, 20)}...</p>`
+  <p>$${this.price}<br>
+  <small><i>${this.location}</i></small></p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
